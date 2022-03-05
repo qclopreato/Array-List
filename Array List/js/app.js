749cc099673418d72207a4c1543bb224811ac9e0
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-03-05 12:13:20 -0600

    Update app.js

--- Array List/js/app.js
+++ Array List/js/app.js
@@ -8,20 +8,19 @@ document.getElementById(`submit`).addEventListener(`click`, function(){
     for (i=0;i<5;i++){
           console.log(myArray[i]);
         if(i==0){
-            document.getElementById(`firstOut`).innerHTML = myArray[i];
+            document.getElementById(`firstOut`).innerHTML = `1. ` + myArray[i];
         }
         if(i==1){
-            document.getElementById(`secondOut`).innerHTML = myArray[i];
+            document.getElementById(`secondOut`).innerHTML = `2. ` + myArray[i];
         }
         if(i==2){
-            document.getElementById(`thirdOut`).innerHTML = myArray[i];
+            document.getElementById(`thirdOut`).innerHTML = `3. ` + myArray[i];
         }
         if(i==3){
-            document.getElementById(`fourthOut`).innerHTML = myArray[i];
+            document.getElementById(`fourthOut`).innerHTML = `4. ` + myArray[i];
         }
         if(i==4){
-            document.getElementById(`fifthOut`).innerHTML = myArray[i];
+            document.getElementById(`fifthOut`).innerHTML = `5. ` + myArray[i];
         }
     }
 });
-
